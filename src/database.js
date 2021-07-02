834a8bd29f379f31b7a4dbafd90e625bde7d771a
Eduardo Sá Neto <eduardo.albuquerque94@gmail.com>
2021-07-02 17:59:30 -0300

    Preparo para o deploy

--- src/database.js
+++ src/database.js
@@ -1,13 +1,28 @@
-import pg from "pg";
+// import pg from "pg";
+
+// const { Pool } = pg;
+
+// const connection = new Pool({
+//   host: "localhost",
+//   port: 5432,
+//   password: "123456",
+//   database: "project_kapyvara",
+//   user: "postgres",
+// });
+
+// export default connection;
+
+import pg from 'pg';
 
 const { Pool } = pg;
 
-const connection = new Pool({
-  host: "localhost",
-  port: 5432,
-  password: "123456",
-  database: "project_kapyvara",
-  user: "postgres",
-});
+const databaseConfig = {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
+
+const connection = new Pool(databaseConfig);
 
 export default connection;
